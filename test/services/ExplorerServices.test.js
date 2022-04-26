25b1af1da062230b74ea0a996be41b1db990dc25
R-V-L <Ricardo_vl@outlook.com>
2022-04-25 23:56:14 -0700

    se arregla fallo para ver que funcione actions

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -4,6 +4,6 @@ describe("Test para explorer service", () => {
     test("Requerimento 1: calcular todos los explorers en una mision", () => {
         const explorers = [{mission: "node"}]
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(2)
+        expect(explorersInNode.length).toBe(1)
     })
 })
